41d8a8228e1f45ba15365903940f554578914d1c
Ryan-Cruiz <ryancruiz97@gmail.com>
2022-08-15 02:25:38 +0800

    master

--- codes/application/models/Admin.php
+++ codes/application/models/Admin.php
@@ -53,23 +53,16 @@ class Admin extends CI_Model{
     public function update_product(){
         
     }
-<<<<<<< HEAD
-=======
     /* UPDATE CATEGORY */
->>>>>>> develop
+
     public function update_category($id,$input){
         return $this->db->query('UPDATE categories SET category = ?, updated_at = NOW() WHERE id = ?',
         array($this->security->xss_clean($input),$this->security->xss_clean($id)));
     }
-<<<<<<< HEAD
-    public function delete_category($id){
-        return $this->db->query('DELETE FROM categories WHERE id = ?',array($this->security->xss_clean($id)));
-    }
-=======
+    
     /* DELETE CATEGORY QUERY */
     public function delete_category($id){
         return $this->db->query('DELETE FROM categories WHERE id = ?',array($this->security->xss_clean($id)));
     }
- 
->>>>>>> develop
+
 }
